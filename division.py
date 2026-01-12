1fb6f6a1cf3aa8cffe048bbba2ff894c52e16342
harunmaina-1129 <harunnmainaaa@gmail.com>
2026-01-11 22:05:13 -0800

    Add files via upload

---
+++ division.py
@@ -0,0 +1,5 @@
+def divide_numbers(a,b):
+ if b==0:
+   return"division by zero not allowed"
+ return a/b
+print(divide_numbers(10,7))
---
+++ subtraction.py
@@ -0,0 +1,3 @@
+def subtract_numbers(g,y):
+    return(g-y)
+print(subtract_numbers(14,5))
